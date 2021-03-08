d7837c1a8ebf3c645a2e3661891ece139b7bf427
Adria <adria.soce@gmail.com>
2021-03-08 20:12:29 +0100

    Activity Receta

---
+++ app/src/main/java/com/cooknow/cooknow/RecetaActivity.kt
@@ -0,0 +1,15 @@
+package com.cooknow.cooknow
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class RecetaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_receta)
+
+    }
+}
