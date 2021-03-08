b94e512a45210ea297cee5fb551736f836c3c777
Stefano <garys.2025@gmail.com>
2021-03-08 02:19:03 +0100

    Las fichas estan apiladas y borran una a una con boton

--- app/src/main/java/com/cooknow/cooknow/DiarioAdapter.kt
+++ app/src/main/java/com/cooknow/cooknow/DiarioAdapter.kt
@@ -3,11 +3,12 @@ package com.cooknow.cooknow
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
 import com.cooknow.cooknow.classes.RecetaKt
 import kotlinx.android.synthetic.main.receta_item.view.*
 
-class DiarioAdapter(val receta:List<RecetaKt>):RecyclerView.Adapter<DiarioAdapter.DiarioHolder>() {
+class DiarioAdapter(val receta:MutableList<RecetaKt>):RecyclerView.Adapter<DiarioAdapter.DiarioHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DiarioHolder {
@@ -17,14 +18,25 @@ class DiarioAdapter(val receta:List<RecetaKt>):RecyclerView.Adapter<DiarioAdapte
 
 
     override fun getItemCount(): Int {
-        return 3
+        return receta.size
     }
 
     override fun onBindViewHolder(holder: DiarioHolder, position: Int) {
         holder.render(receta[position])
+        holder.view.botonRechazar.setOnClickListener(object : View.OnClickListener {
+
+            override fun onClick(view:View){
+                receta.removeAt(position)
+                notifyItemRemoved(position)
+                notifyItemRangeChanged(position, receta.size)
+            }
+        })
     }
 
+
     class DiarioHolder(val view:View):RecyclerView.ViewHolder(view){
+
+
 
         fun render(receta: RecetaKt){
             view.nombreReceta.text = receta.getNombre()
@@ -32,7 +44,11 @@ class DiarioAdapter(val receta:List<RecetaKt>):RecyclerView.Adapter<DiarioAdapte
             view.duracionReceta.text = receta.getDuracion()
             view.personasReceta.text = receta.getPersonas()
             view.dificultadReceta.numStars = receta.getDificultad()
+            view.popularidadReceta.text = receta.getPopularidad()
         }
+
+
+
     }
 
 }
--- app/src/main/java/com/cooknow/cooknow/DiarioFragment.kt
+++ app/src/main/java/com/cooknow/cooknow/DiarioFragment.kt
@@ -5,23 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.cooknow.cooknow.classes.OverlappingDecoration
 import com.cooknow.cooknow.classes.RecetaKt
 import kotlinx.android.synthetic.main.fragment_diario.*
 
 class DiarioFragment : Fragment() {
 
-    var recetas: List<RecetaKt> = listOf(
-        RecetaKt("Tortilla", "x", "Esta receta viene de España y ha ganado mucha popularidad porque y...", "20", "10 minutos", "2 comensales", 3),
+    var recetas: MutableList<RecetaKt> = mutableListOf(
+        RecetaKt("Tortilla de patatas", "x", "Esta receta viene de España y ha ganado mucha popularidad porque y...", "20", "10 minutos", "2 comensales", 3),
         RecetaKt("Macarrones", "71", "Esta de Italia x y lo hace todo el mundo porque es facil...", "20", "6 minutos", "3 comensales", 4),
         RecetaKt("Pizza 4 quesos", "4", "Esta receta viene de EEUU y ha ganado mucha popularidad porque y lo han vendido muy bien por todo el mundo, no toma mucho tiempo y esta muy buena. Creemos que te podria encantar. muchos animos...", "20", "15 minutos", "5 comensales", 5),
-        RecetaKt("Tortilla", "8", "Paso 1", "20", "4", "3", 1),
-        RecetaKt("Tortilla", "2", "Paso 1", "20", "4", "3", 3),
-        RecetaKt("Tortilla", "Paso 1", "Paso 1", "20", "4", "3", 2),
-        RecetaKt("Tortilla", "Paso 1", "Paso 1", "20", "4", "3", 4),
-        RecetaKt("Tortilla", "Paso 1", "Paso 1", "20", "4", "3", 1)
+        RecetaKt("Ensalada", "8", "Esta receta es universal y ha ganado mucha popularidad estos ultimos añosporque y", "20", "8 minutos", "1 comensal", 1),
+        RecetaKt("Arroz con pollo", "2", "Esta de Colombia x y lo hace todo el mundo porque es facil y sano para mejorar la condicion fisica. Preparala ya", "20", "20 minutos", "3 comensales", 3),
+        RecetaKt("Fideos", "Paso 1", "Esta receta viene de Japon y ha ganado mucha popularidad porque y lo han vendido muy bien por todo el mundo, no toma mucho tiempo y esta muy buena. Creemos que", "20", "40 minutos", "2 comensales", 2),
+        RecetaKt("Paella", "Paso 1", "Reseta clasica de España que deberia probar todo el mundo. Tomara mucho tiempo", "20", "80 minutos", "8 comensales", 4),
+        RecetaKt("Pastel de chocolate", "Paso 1", "Un postre de chocolate para celebrar en una fiesta", "20", "25 minutos", "5 comensales", 1)
     )
 
     override fun onCreateView(
@@ -30,13 +32,9 @@ class DiarioFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_diario, container, false)
-
-
         // IMPLEMENT
 
         (activity as AppCompatActivity?)!!.title = "Diario"
-
-
 
 
         return view
@@ -46,11 +44,17 @@ class DiarioFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         initRecyclerRecetario()
+
+
+
     }
 
     fun initRecyclerRecetario(){
+        recetas.shuffle()
+        var subRecetas = recetas.subList(1, 5)
+        recyclerView.addItemDecoration(OverlappingDecoration())
         recyclerView.layoutManager = LinearLayoutManager(activity)
-        val adapter = DiarioAdapter(recetas)
+        val adapter = DiarioAdapter(subRecetas)
         recyclerView.adapter = adapter
     }
 
